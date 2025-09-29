49b1285f384578a81d941bd9109cda4203c57ebf
roychoi <roychoi1012@naver.com>
2025-09-29 12:53:46 +0900

    feat: 코드 추가

--- myapi/settings.py
+++ myapi/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myapi.urls'
@@ -122,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
--- quiz/urls.py
+++ quiz/urls.py
@@ -3,5 +3,5 @@ from .views import helloAPI, randomQuiz
 
 urlpatterns = [
     path("hello/", helloAPI),
-    path("<int:id>/", randomQuiz),
+    path("random/<int:id>/", randomQuiz),
 ]
